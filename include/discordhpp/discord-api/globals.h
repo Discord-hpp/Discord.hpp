675e1160893dfcc3843679666dfc94826a185f75
yuto0214w <yuto0214w@gmail.com>
2022-06-28 16:15:38 +0900

    fix regex

--- include/discordhpp/discord-api/globals.h
+++ include/discordhpp/discord-api/globals.h
@@ -8,15 +8,15 @@ typedef std::string permissions;
 
 struct FormattingPatterns
 {
-	std::regex user();						/* /<@(?<id>\d{17,20})>/ */
-	std::regex UserWithNickname();			/* /<@!(?<id>\d{17,20})>/ */
-	std::regex UserWithOptionalNickname();	/* /<@!?(?<id>\d{17,20})>/ */
-	std::regex Channel();					/* /<#(?<id>\d{17,20})>/ */
-	std::regex Role();						/* /<@&(?<id>\d{17,20})>/ */
-	std::regex Emoji();						/* /<(?<animated>a)?:(?<name>\w{2,32}):(?<id>\d{17,20})>/ */
-	std::regex AnimatedEmoji();				/* /<(?<animated>a):(?<name>\w{2,32}):(?<id>\d{17,20})>/ */
-	std::regex StaticEmoji();				/* /<:(?<name>\w{2,32}):(?<id>\d{17,20})>/ */
-	std::regex Timestamp();					/* /<t:(?<timestamp>-?\d{1,13})(:(?<style>[tTdDfFR]))?>/ */
-	std::regex DefaultStyledTimestamp();	/* /<t:(?<timestamp>-?\d{1,13})>/ */
-	std::regex StyledTimestamp();			/* /<t:(?<timestamp>-?\d{1,13}):(?<style>[tTdDfFR])>/ */
+	std::regex User(const char* =                     R"(<@(?<id>\d{17,20})>)");
+	std::regex UserWithNickname(const char* =         R"(<@!(?<id>\d{17,20})>)");
+	std::regex UserWithOptionalNickname(const char* = R"(<@!?(?<id>\d{17,20})>)");
+	std::regex Channel(const char* =                  R"(<#(?<id>\d{17,20})>)");
+	std::regex Role(const char* =                     R"(<@&(?<id>\d{17,20})>)");
+	std::regex Emoji(const char* =                    R"(<(?<animated>a)?:(?<name>\w{2,32}):(?<id>\d{17,20})>)");
+	std::regex AnimatedEmoji(const char* =            R"(<(?<animated>a):(?<name>\w{2,32}):(?<id>\d{17,20})>)");
+	std::regex StaticEmoji(const char* =              R"(<:(?<name>\w{2,32}):(?<id>\d{17,20})>)");
+	std::regex Timestamp(const char* =                R"(<t:(?<timestamp>-?\d{1,13})(:(?<style>[tTdDfFR]))?>)");
+	std::regex DefaultStyledTimestamp(const char* =   R"(<t:(?<timestamp>-?\d{1,13})>)");
+	std::regex StyledTimestamp(const char* =          R"(<t:(?<timestamp>-?\d{1,13}):(?<style>[tTdDfFR])>)");
 };
